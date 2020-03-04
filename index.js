fb2e23be6834c375f35d8b027cb46427f388222f
Maciej Maciejewski <maciej.maciejewski@finastra.com>
2020-03-04 08:42:02 +0100

    Add hook

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+require('dotenv').config()
+
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/hello', function (req, res) {
   return res.status(200).send("hello")
 })
 
-app.listen(process.env.PORT)
+app
+  .post('/webhook/inbound', (req, res) => {
+    const params = Object.assign(request.query, request.body)
+    console.log(params)
+    res.status(204).send()
+  })
+
+app.listen(process.env.PORT, () => {
+  console.log('Starting app')
+})
